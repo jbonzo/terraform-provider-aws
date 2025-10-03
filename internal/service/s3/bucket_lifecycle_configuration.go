a6f765c558a2e086dc3d6983e5b160e4dc6141e4
jbonzo <8647805+jbonzo@users.noreply.github.com>
2025-10-03 14:49:21 -0400

    Use aws sdk for pointer conversion

--- internal/service/s3/bucket_lifecycle_configuration.go
+++ internal/service/s3/bucket_lifecycle_configuration.go
@@ -650,7 +650,7 @@ func findBucketLifecycleConfiguration(ctx context.Context, conn *s3.Client, buck
 	for i := range output.Rules {
 		rule := &output.Rules[i]
 		//nolint:staticcheck // Yes the attribute Prefix is deprecated, but the following functionality is required for compatibility with non AWS systems
-		if (*rule).Prefix != nil && *(*rule).Prefix == "" {
+		if aws.ToString((*rule).Prefix) == "" {
 			//nolint:staticcheck // Yes the attribute Prefix is deprecated, but the following functionality is required for compatibility with non AWS systems
 			(*rule).Prefix = nil
 		}
